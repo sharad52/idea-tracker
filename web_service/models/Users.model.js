80a72ba36bf4d58e21170a2a9d78723cff16f6a5
sharad52 <chemist.sharad@gmail.com>
2023-05-16 12:13:36 +0545

    server: password hashing using bcrypt algorithm

--- web_service/models/Users.model.js
+++ web_service/models/Users.model.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const userHistorySchema = require("./UserHistory.model");
+const bcrypt = require("bcrypt");
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -46,6 +48,57 @@ const userSchema = new mongoose.Schema({
     
 });
 
+
+// add property history handling 
+
+const fieldsWithHistory =  ["firstName", "lastName", "rank", "email"];
+userSchema.pre("findOneAndUpdate", async function (next) {
+    const updatedUser = this.getUpdate();
+    const currentUser = await this.model.findOne(this.getQuery());
+
+    for (const field of fieldsWithHistory) {
+        if (updatedUser[field]) {
+            if (updatedUser[field] !== currentUser[field]) {
+                updatedUser.propertyHistory = {
+                    ...updatedUser.propertyHistory,
+                    [`${field}History`]: [
+                        { value: updatedUser[field], whenModified: Date.now() },
+                        ...currentUser.propertyHistory[`${field}History`],
+                    ],
+                };
+            }
+        }
+        next();
+    }
+});
+
+userSchema.pre("save", function (next) {
+    var user = this;
+
+    if (!user.isModified("password")) return next();
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const isMatch = await bcrypt.compare(candidatePassword, this.password);
+            resolve(isMatch);
+        } catch(err) {
+            reject(err);
+        }
+    });
+};
 
 const User = mongoose.model("User", userSchema);
 
