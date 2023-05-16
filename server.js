f4af23a965574ca20e7d84bafe7192c0cc916582
sharad52 <chemist.sharad@gmail.com>
2023-05-16 10:46:22 +0545

    server: routes and controllers

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ if (process.env.NODE_ENV != "production") {
 // Import dependencies
 const express = require("express");
 const connectToDb = require("./web_service/config/connectToDb");
+const userController = require("./web_service/controllers/userController");
 
 // create an express app
 const app = express();
@@ -17,6 +18,13 @@ connectToDb();
 app.get('/', (req, res) => {
     res.json({hello: "world"});
 })
+app.get("/users", userController.fetchAllUser);
+
+app.post("/user", userController.createUser);
+
+app.put("/user/:id", userController.updateUser);
+
+app.delete("user/:id", userController.deleteUser);
 
 // start our server
 app.listen(process.env.PORT);
--- web_service/controllers/userController.js
+++ web_service/controllers/userController.js
@@ -1,3 +1,33 @@
 const express = require("express");
 const axios = require("axios");
 const Users = require("../models/Users.model");
+
+
+const fetchAllUser = async (req, res) => {
+    try {
+        const users = await Users.find({});
+        res.send(users);
+    } catch(err) {
+        next(err);
+    }
+}
+
+const createUser = async (req, res) => {
+    console.log("Creating User");
+}
+
+const updateUser = async (req, res) => {
+    console.log("Updating User......");
+}
+
+const deleteUser = async (req, res) => {
+    console.log("Deleting User.....");
+}
+
+
+module.exports = {
+    fetchAllUser: fetchAllUser,
+    createUser: createUser,
+    updateUser: updateUser,
+    deleteUser: deleteUser,
+};
